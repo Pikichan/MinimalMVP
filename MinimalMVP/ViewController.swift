8f411a187cc20706714333dc1874324a64b9fb7b
Pikichan <pijimjim@gmail.com>
2021-08-01 18:25:35 +0900

    最初のコミット

---
+++ MinimalMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MinimalMVP
+//
+//  Created by 菊辻みやこ on 2021/08/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
